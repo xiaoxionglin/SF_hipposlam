2637cc8000e3da07e12e42f9d067c37ac232d2b9
Xiao-Xiong Lin <ln.xxng@gmail.com>
2026-09-22 19:13:49 +0200

    Add batched HER release qualifications

---
+++ hpc_runs/ca3_state_goal_followup_preflight_v3.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+from hpc_runs.intrmotiv_study import load_study
+from hpc_runs.intrmotiv_study.sample_factory import build_run_description
+
+SPEC = Path(__file__).with_name("studies") / "ca3_state_goal_followup_20260922_preflight_v3.study.json"
+STUDY = load_study(SPEC)
+RUN_DESCRIPTION = build_run_description(STUDY)
